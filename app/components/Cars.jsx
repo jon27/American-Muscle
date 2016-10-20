af9ad987019dad8edb2822ae580eb5909fb84839
jon cabre <jon.cabre@gmail.com>
2016-10-20 11:39:06 -0400

    Removed Bug in American muscle search-console log issue

--- app/components/Cars.jsx
+++ app/components/Cars.jsx
@@ -19,7 +19,6 @@ componentDidUpdate: function(prevProps, prevState){
 renderCarByYear: function(key){
    var year = key;
    var cars = require("./NewArray.jsx");
-   console.log("i am the year input from carform passed to the renderCarByYear " + year);
    function findCarByYear(car){
     return car.year === key;
    }
